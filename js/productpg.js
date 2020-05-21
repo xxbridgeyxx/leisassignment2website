cd9db23e1f00656d45e9ba29e84bcdec8519c83a
Bridge Mikaera <mikaeraboo@Bridges-MacBook-Air.local>
2020-05-21 15:20:58 +1200

    somthing

---
+++ js/productpg.js
@@ -0,0 +1,72 @@
+//add product to cart
+function addProductToCart(buttonID) {
+    cart = document.getElementById("rightDiv");
+    switch (buttonID) {
+        case "addWorm":
+            qty = document.getElementById("wormQty").value;
+            addHTML = document.createElement("p");
+            addHTML.className="orderedProducts";
+            addHTML.innerHTML = "Worms: "+qty+" Price: <strong class='price'>" +(2*Number(qty)) +"</strong>";
+            console.log(addHTML);
+            cart.insertBefore(addHTML, document.getElementById("checkOut"));
+            break;
+        case "addMilk":
+            qty = document.getElementById("milkQty").value;
+            addHTML = document.createElement("p");
+            addHTML.className="orderedProducts";
+            addHTML.innerHTML = "Milk: "+qty+" Price: <strong class='price'>"+(3*Number(qty)) +"</strong>";
+            console.log(addHTML);
+            cart.insertBefore(addHTML, document.getElementById("checkOut"));
+            break;
+    }
+}
+
+//Popup Invoice
+
+function invoice() {
+
+    modal = document.getElementById("myModal");
+    modal.style.display = "block";
+
+    // Get the <span> element that closes the modal
+    cross = document.getElementsByClassName("close")[0];
+    console.log(cross);
+
+
+    cross.onclick = function() {
+        modal.style.display = "none";
+    }
+
+
+// When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
+
+    //Take everything from shopping cart
+    cart = document.getElementById("rightDiv");
+    items = document.getElementsByClassName("orderedProducts");
+    //invoicePlace is the place we add items in
+    invoicePlace = document.getElementById("invoicePlace");
+    i = 0;
+    text = "<h2>Invoice</h2>";
+    while (i<items.length){
+        text += "<p>"+items[i].innerHTML+"</p>";
+        i++;
+    }
+
+    //calculate total
+    total = 0;
+    i = 0;
+    prices = document.getElementsByClassName("price");
+    while (i<prices.length){
+        console.log(prices[i].innerText);
+        total += Number(prices[i].innerText);
+        i++;
+    }
+    text += "<p>Total: $"+total+"</p>";
+    invoicePlace.innerHTML = text;
+}
